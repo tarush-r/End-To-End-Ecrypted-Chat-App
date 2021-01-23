917d16fdd682a9c75484428513b9e6256dbbce18
sakship31 <pandey.sk@somaiya.edu>
2021-01-23 23:29:42 +0530

    encryption-decryption of private key done

--- chatapp_client/lib/helpers/encryption_helper.dart
+++ chatapp_client/lib/helpers/encryption_helper.dart
@@ -3,8 +3,8 @@ import 'dart:convert';
 import 'package:rsa_encrypt/rsa_encrypt.dart';
 import 'package:pointycastle/api.dart' as encryption;
 import 'package:pointycastle/export.dart' as pointy;
-
 import 'package:asn1lib/asn1lib.dart';
+import 'package:encrypt/encrypt.dart';
 import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
@@ -14,9 +14,43 @@ class EncryptionHelper {
     var bytes = utf8.encode(password); // data being hashed
 
     var digest = sha1.convert(bytes).toString();
-    return digest.substring(0, 20);
+    List<String> divided_hashed_password = [];
+    divided_hashed_password.add(digest.substring(0, 23));
+    divided_hashed_password.add(digest.substring(24, 40));
+    return divided_hashed_password;
     //api to send pass to server
     // print(digest.substring(20, 40));
+  }
+
+  static encryptPrivateKey(
+      String key_hashed_password, String text_private_key) {
+    final plainText = text_private_key;
+    final key = Key.fromUtf8(key_hashed_password);
+    final iv = IV.fromUtf8("bfdcjjnjfnf");
+    final encrypter = Encrypter(AES(key));
+    final encrypted = encrypter.encrypt(plainText, iv: iv);
+    print("PLAIN");
+    print(plainText); //generated private key (considered as plain text to be encrypted)
+    print("ENCRYPTED");
+    print(encrypted.base64); //encrypted private key in string format
+    return encrypted.base64;
+  }
+
+  static decryptPrivateKey(
+      String key_hashed_password, String encrypted_private_key) {
+    final key = Key.fromUtf8(key_hashed_password);
+    final iv = IV.fromUtf8("bfdcjjnjfnf");
+
+    final encrypter = Encrypter(AES(key));
+
+    // final encrypted = encrypter.encrypt(plainText, iv: iv);
+    final decrypted =
+        encrypter.decrypt(Encrypted.fromBase64(encrypted_private_key), iv: iv);
+    print("ENCRYPTED");
+    print(encrypted_private_key); //encrypted private key in string format
+    print("DECRYPTED");
+    print(decrypted); //decrypted private key(which was earlier passed as plain text)
+    return decrypted;
   }
 
   static Future<
@@ -78,7 +112,7 @@ class EncryptionHelper {
 
   static String convertPrivateKeyToString(pointy.RSAPrivateKey privateKey) {
     var topLevel = ASN1Sequence();
- 
+
     var version = ASN1Integer(BigInt.from(0));
     var modulus = ASN1Integer(privateKey.n);
     var publicExponent = ASN1Integer(privateKey.exponent);
@@ -91,7 +125,7 @@ class EncryptionHelper {
     var exp2 = ASN1Integer(dQ);
     var iQ = privateKey.q.modInverse(privateKey.p);
     var co = ASN1Integer(iQ);
- 
+
     topLevel.add(version);
     topLevel.add(modulus);
     topLevel.add(publicExponent);
@@ -101,9 +135,9 @@ class EncryptionHelper {
     topLevel.add(exp1);
     topLevel.add(exp2);
     topLevel.add(co);
- 
+
     var dataBase64 = base64.encode(topLevel.encodedBytes);
- 
+
     return "$dataBase64";
   }
 
@@ -112,18 +146,14 @@ class EncryptionHelper {
     final exponent = (sequence.elements[3] as ASN1Integer).valueAsBigInteger;
     final p = (sequence.elements[4] as ASN1Integer).valueAsBigInteger;
     final q = (sequence.elements[5] as ASN1Integer).valueAsBigInteger;
- 
+
     return pointy.RSAPrivateKey(modulus, exponent, p, q);
   }
 
   static pointy.RSAPrivateKey convertStringToPrivateKey(String key) {
     final rows = key.split('\n'); // LF-only, this could be a problem
     final header = rows.first;
-    
- 
-      return _parsePrivate(_parseSequence(rows));
- 
+
+    return _parsePrivate(_parseSequence(rows));
   }
-
-
 }
--- chatapp_client/lib/screens/register_screen.dart
+++ chatapp_client/lib/screens/register_screen.dart
@@ -4,13 +4,13 @@ import 'package:rsa_encrypt/rsa_encrypt.dart';
 import 'package:pointycastle/api.dart' as encryption;
 import '../api/authentication_api.dart';
 import 'package:pointycastle/export.dart' as pointy;
+import 'package:encrypt/encrypt.dart';
 
 import 'package:asn1lib/asn1lib.dart';
 
 import 'dart:convert';
 import 'dart:math';
 import 'dart:typed_data';
-
 
 // import 'package:crypto/crypto.dart';
 // import 'dart:convert';
@@ -26,34 +26,46 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   final GlobalKey<FormState> _formKey = GlobalKey();
   TextEditingController passwordController = new TextEditingController();
-  
+
   _submit() async {
     final isValid = _formKey.currentState.validate();
     if (!isValid) {
       return;
     }
     _formKey.currentState.save();
-    String hashedPassword = EncryptionHelper.hashPassword(passwordController.text);
-    encryption.AsymmetricKeyPair keyPair = await EncryptionHelper.generateKeyPair();
-    
+    List<String> divided_hashed_password =
+        EncryptionHelper.hashPassword(passwordController.text);
+    encryption.AsymmetricKeyPair keyPair =
+        await EncryptionHelper.generateKeyPair();
+
     //--------------------------------------------------
     //YEH API KO UNCOMMENT MAT KARNA NAHI TOH ERROR AEGA
     //--------------------------------------------------
-    
+
     // AuthenticationApi.register(hashedPassword, keyPair.publicKey, keyPair.privateKey);
     print(EncryptionHelper.convertPublicKeyToString(keyPair.publicKey));
     print("DECODING");
-    print(EncryptionHelper.convertStringToPublicKey(EncryptionHelper.convertPublicKeyToString(keyPair.publicKey)));
+    print(EncryptionHelper.convertStringToPublicKey(
+        EncryptionHelper.convertPublicKeyToString(keyPair.publicKey)));
     print(EncryptionHelper.convertPublicKeyToString(keyPair.publicKey));
     var x = encrypt("HELLO", keyPair.publicKey);
     print(EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey));
     print("DECODING");
-    print(EncryptionHelper.convertStringToPrivateKey(EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey)));
+    print(EncryptionHelper.convertStringToPrivateKey(
+        EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey)));
     print(EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey));
-    pointy.PrivateKey pk = EncryptionHelper.convertStringToPrivateKey(EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey));
+    pointy.PrivateKey pk = EncryptionHelper.convertStringToPrivateKey(
+        EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey));
     print(decrypt(x, keyPair.privateKey));
-    // _hashPassword(passwordController.text);
-    // print(passwordController.text);
+    print("HASHED");
+    print(divided_hashed_password);
+    //encrypted_private_key below is stored in database
+    String encrypted_private_key = EncryptionHelper.encryptPrivateKey(
+        divided_hashed_password[1],
+        EncryptionHelper.convertPrivateKeyToString(keyPair.privateKey));
+    //decrypted_private_key below is to be stored in local storage. This key is decrpted upon logging in
+    String decrypted_private_key = EncryptionHelper.decryptPrivateKey(
+        divided_hashed_password[1], encrypted_private_key);
   }
 
   bool passwordHide = false;
